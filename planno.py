df55e2fcb58460411ebedf94ff14098658dfe5bd
Ricky Peddicord <46783411+rickypeddicord@users.noreply.github.com>
2022-01-30 18:19:08 -0500

    Add files via upload

---
+++ planno.py
@@ -0,0 +1,99 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+import psycopg2
+import psycopg2.extras
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+
+        # Create database table if it doesn't exist
+        conn = psycopg2.connect(
+            host = "ec2-34-205-209-14.compute-1.amazonaws.com",
+            database = "d19re7njihace8",
+            user = "lveasasuicarlg",
+            password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
+            port = "5432",
+        )
+
+        # Create a cursor
+        c = conn.cursor()
+
+        # Create a table
+        c.execute("CREATE TABLE if not exists users(id SERIAL PRIMARY KEY, username VARCHAR(255), password VARCHAR(255))")
+
+        conn.commit()
+        conn.close()
+
+        return Builder.load_file('login.kv')
+        
+    def login(self):
+        loginCode = -1
+        conn = psycopg2.connect(
+            host = "ec2-34-205-209-14.compute-1.amazonaws.com",
+            database = "d19re7njihace8",
+            user = "lveasasuicarlg",
+            password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
+            port = "5432",
+        )
+
+        c = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        query = "SELECT * FROM users WHERE username = %s"
+        c.execute(query, (self.root.ids.user.text,))
+        records = c.fetchall()
+        
+        if records:
+            if records[0][1] == self.root.ids.user.text and records[0][2] == self.root.ids.password.text:
+                self.root.ids.welcome_label.text = "Logged in successfully"
+                loginCode = 1
+            else:
+                self.root.ids.welcome_label.text = "User doesn't exist or incorrect password entered"
+                loginCode = -1
+        else:
+            self.root.ids.welcome_label.text = "User doesn't exist or incorrect password entered"
+            loginCode = -1
+
+        conn.commit()
+        conn.close()
+        return loginCode
+    
+    def clear(self):
+        self.root.ids.welcome_label.text = "Please Login or Register"
+        self.root.ids.user.text = ""
+        self.root.ids.password.text = ""
+
+    def register(self):
+        conn = psycopg2.connect(
+            host = "ec2-34-205-209-14.compute-1.amazonaws.com",
+            database = "d19re7njihace8",
+            user = "lveasasuicarlg",
+            password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
+            port = "5432",
+        )
+
+        c = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        if self.root.ids.user.text == "" and self.root.ids.password.text == "":
+            print("fields cannot be empty")
+            return
+        else:
+            c.execute("SELECT * FROM users")
+            records = c.fetchall()
+            print(records)
+        if records:
+            for record in records:
+                if record[1] == self.root.ids.user.text:
+                    self.root.ids.welcome_label.text = "An account with these credentials already exists"
+                else:
+                    c.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (self.root.ids.user.text, self.root.ids.password.text))
+                    self.root.ids.welcome_label.text = "Account created successfully"
+                    break
+        else:
+            c.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (self.root.ids.user.text, self.root.ids.password.text))
+            self.root.ids.welcome_label.text = "Account created successfully"
+        conn.commit()
+        conn.close()
+
+MainApp().run()
